e9e4a81cf1bb740840c99ed95673569f98c42ec5
MoXuanSAMA <2809603169@qq.com>
2026-06-18 12:45:46 +0800

    新增 core 的 pyi 文件

---
+++ example_bp/example_scripts/easy/core.pyi
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+from mod.client.system.clientSystem import ClientSystem
+from mod.common.mod import Mod
+from mod.server.system.serverSystem import ServerSystem
+from typing import Callable, List, Optional, Type, Union
+
+
+class Easy(Mod):
+    name: str
+    version: Optional[str]
+    dist: List[int]
+
+    def __init__(self, name: str, version: Optional[str] = None, dist: Optional[List[int]] = None) -> None:
+        ...
+
+    def __call__(self, cls: Type[EventBus]) -> Type[EventBus]:
+        ...
+
+
+class Event:
+    name: str
+    priority: int
+    custom: bool
+    broadcast: bool
+    dist: List[int]
+
+    def __init__(self, name: str, priority: int = 0, custom: bool = False, broadcast: bool = False, dist: Optional[List[int]] = None) -> None:
+        ...
+
+    def __call__(self, func: Callable) -> Callable:
+        ...
+
+
+class ServerEvent(Event):
+    ...
+
+
+class ClientEvent(Event):
+    ...
+
+
+class EventBus:
+    system: Union[ServerSystem, ClientSystem]
